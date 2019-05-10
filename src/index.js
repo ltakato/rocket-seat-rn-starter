8beccdae92c81e3b7d19ea901ff11fef781a5e54
takato <leonardo.takato@soaexpert.com.br>
2019-05-10 02:10:52 -0300

    feito o curso daora de react native starter da rocketseat

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Routes from './routes';
+import './config/statusBarConfig';
+
+const App = () => <Routes />;
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import { createStackNavigator } from 'react-navigation';
+
+import Main from './pages/main';
+import Product from './pages/product';
+
+export default createStackNavigator({
+  Main,
+  Product
+}, {
+  navigationOptions: {
+    headerStyle: {
+      backgroundColor: "#DA552F"
+    },
+    headerTintColor: "#FFF"
+  }
+});
